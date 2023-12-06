49b478872802a865eef85e8f8ee7663b6ef3246a
Eduardo Corpeño <kuashio@gmail.com>
2023-12-06 14:52:42 +0000

    getting the code camera ready

--- src/06_03/console.asm
+++ src/06_03/console.asm
@@ -2,12 +2,12 @@
 # Exercise 06_03
 # Console Interaction, by Eduardo Corpeño 
 
-###################################################################
+###################################################
 # Description
 #
-# This assembly code asks the user's name in the console
-# and responds with a message using the entered name.
-###################################################################
+# This code asks the user's name in the console 
+# and responds with a message using that name.
+###################################################
 
 # Data Section
 .data
@@ -18,15 +18,14 @@ name:     .string   "                       "
 # Code Section
 .text
 
-###################################################################
+###################################################
 # Main entry point.
 # The program starts here, at address 0x00000000
-###################################################################
+###################################################
 
 main:
     # Initializations
-    # t0 is a pointer to the name string
-    la t0, name
+    la t0, name  # t0 points to the name string
 
     # print_string(prompt) - Environment call 4
     la a1, prompt
@@ -55,20 +54,21 @@ main:
     ecall
     ecall
 
-    # Exit - Environment call 17
-    addi a0, x0, 17
+    # Exit - Environment call 10
+    li a0, 10
     ecall
     
 
-###################################################################
+###################################################
 # read_str subroutine
 # Read input string from the console.
-# This input is a line of text terminated with the enter keystroke.
-###################################################################
+# This input is a line of text terminated with
+# the enter keystroke.
+###################################################
 
 read_str:
     # Initializations
-    li a5, 1     # a5 holds comparison value for branching
+    li a5, 1  # a5 holds comparison value for branching
 
     # Enable console input - Environment call 0x130
     li a0, 0x130 
@@ -80,16 +80,17 @@ read_char:
     ecall
 
     # Read the result of the environment call in a0
-    beq a0, a5, read_char # If still waiting for input, keep polling
-    beq a0, x0, finish    # If buffer is empty, go to finish
+    beq a0, a5, read_char   # If still waiting for input, keep polling
+    beq a0, zero, finish    # If buffer is empty, go to finish
     
     # Handle incoming character
     sb   a1, 0(t0)        # Append input character to name string 
     addi t0, t0, 1        # Increment the name string pointer
 
-    j read_char           # Iterate to get the next character
+    # Iterate to get the next character
+    j read_char
 
 finish:
-    # Suboutine epoligue
+    # Subroutine epilogue
     sb zero, 0(t0)  # Append null-terminator to name string
     jr ra           # Return to caller
